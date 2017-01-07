45a768580f676792a91aa7c32164922a945f197f
Jason Chan <jason741852@gmail.com>
2017-01-06 16:32:32 -0800

    added 2nd level

---
+++ Assets/_Scenes/Scripts/Menu/LoadScene.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadScene : MonoBehaviour {
+
+	public void LoadLevel()
+    {
+        //UnityEngine.SceneManagement.SceneManager.LoadScene("MiniGame");
+        Debug.Log("button clicked");
+        Application.LoadLevel(1);
+    }
+}
